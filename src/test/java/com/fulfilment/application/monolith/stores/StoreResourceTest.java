0dac7b5902e43000fb09bc35872799dc1940276c
vchiranjeevi <chiru8750@gmail.com>
2026-01-30 15:12:57 +0530

    <Warehouse>:<added test case changes>

---
+++ src/test/java/com/fulfilment/application/monolith/stores/StoreResourceTest.java
@@ -0,0 +1,116 @@
+package com.fulfilment.application.monolith.stores;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class StoreResourceTest {
+
+    @Test
+    void testCreateStoreEndpoint() {
+        String payload = """
+            {
+              "storeCode": "STR.100",
+              "name": "Electronics Hub",
+              "location": "Dubai",
+              "capacity": 200
+            }
+            """;
+
+        given()
+          .contentType("application/json")
+          .body(payload)
+        .when()
+          .post("/stores")
+        .then()
+          .statusCode(201)
+          .body("storeCode", equalTo("STR.100"))
+          .body("name", equalTo("Electronics Hub"))
+          .body("location", equalTo("Dubai"))
+          .body("capacity", equalTo(200));
+    }
+
+    @Test
+    void testGetStoreEndpoint_NotFound() {
+        given()
+        .when()
+          .get("/stores/STR.999")
+        .then()
+          .statusCode(404);
+    }
+
+    @Test
+    void testUpdateStoreEndpoint() {
+        // First create
+        String payload = """
+            {
+              "storeCode": "STR.101",
+              "name": "Fashion World",
+              "location": "Dubai",
+              "capacity": 150
+            }
+            """;
+
+        given()
+          .contentType("application/json")
+          .body(payload)
+        .when()
+          .post("/stores")
+        .then()
+          .statusCode(201);
+
+        // Update
+        String newPayload = """
+            {
+              "storeCode": "STR.101",
+              "name": "Fashion World Updated",
+              "location": "AbuDhabi",
+              "capacity": 250
+            }
+            """;
+
+        given()
+          .contentType("application/json")
+          .body(newPayload)
+        .when()
+          .put("/stores/STR.101")
+        .then()
+          .statusCode(200)
+          .body("name", equalTo("Fashion World Updated"))
+          .body("location", equalTo("AbuDhabi"))
+          .body("capacity", equalTo(250));
+    }
+
+    @Test
+    void testDeleteStoreEndpoint() {
+        // First create
+        String payload = """
+            {
+              "storeCode": "STR.102",
+              "name": "Book Haven",
+              "location": "Dubai",
+              "capacity": 100
+            }
+            """;
+
+        given()
+          .contentType("application/json")
+          .body(payload)
+        .when()
+          .post("/stores")
+        .then()
+          .statusCode(201);
+
+        // Delete
+        given()
+        .when()
+          .delete("/stores/STR.102")
+        .then()
+          .statusCode(204);
+    }
+}
+
---
+++ src/test/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/WarehouseResourceImplTest.java
@@ -0,0 +1,114 @@
+package com.fulfilment.application.monolith.warehouses.adapters.restapi;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class WarehouseResourceImplTest {
+
+    @Test
+    void testCreateWarehouseEndpoint() {
+        String payload = """
+            {
+              "businessUnitCode": "MWH.700",
+              "location": "Dubai",
+              "capacity": 500,
+              "stock": 200
+            }
+            """;
+
+        given()
+          .contentType("application/json")
+          .body(payload)
+        .when()
+          .post("/warehouses")
+        .then()
+          .statusCode(201)
+          .body("businessUnitCode", equalTo("MWH.700"))
+          .body("location", equalTo("Dubai"))
+          .body("capacity", equalTo(500))
+          .body("stock", equalTo(200));
+    }
+
+    @Test
+    void testGetWarehouseEndpoint_NotFound() {
+        given()
+        .when()
+          .get("/warehouses/MWH.999")
+        .then()
+          .statusCode(404);
+    }
+
+    @Test
+    void testArchiveWarehouseEndpoint() {
+        // First create
+        String payload = """
+            {
+              "businessUnitCode": "MWH.701",
+              "location": "Dubai",
+              "capacity": 300,
+              "stock": 100
+            }
+            """;
+
+        given()
+          .contentType("application/json")
+          .body(payload)
+        .when()
+          .post("/warehouses")
+        .then()
+          .statusCode(201);
+
+        // Then archive
+        given()
+        .when()
+          .delete("/warehouses/MWH.701")
+        .then()
+          .statusCode(204);
+    }
+
+    @Test
+    void testReplaceWarehouseEndpoint() {
+        // First create
+        String payload = """
+            {
+              "businessUnitCode": "MWH.702",
+              "location": "Dubai",
+              "capacity": 300,
+              "stock": 100
+            }
+            """;
+
+        given()
+          .contentType("application/json")
+          .body(payload)
+        .when()
+          .post("/warehouses")
+        .then()
+          .statusCode(201);
+
+        // Replace with new capacity/location
+        String newPayload = """
+            {
+              "businessUnitCode": "MWH.702",
+              "location": "AbuDhabi",
+              "capacity": 400,
+              "stock": 100
+            }
+            """;
+
+        given()
+          .contentType("application/json")
+          .body(newPayload)
+        .when()
+          .put("/warehouses/MWH.702")
+        .then()
+          .statusCode(200)
+          .body("location", equalTo("AbuDhabi"))
+          .body("capacity", equalTo(400));
+    }
+}
